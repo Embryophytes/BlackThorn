a907924bc27249c3d67b0f6cf787c931385d19d8
Tretiakov Yehor <egorken3v@gmail.com>
2024-12-03 18:59:41 +0200

    feature/BTH-33: Add proper viewport movement on ui resize

--- src/viewport/core/state.rs
+++ src/viewport/core/state.rs
@@ -1,4 +1,23 @@
 use bevy::prelude::Resource;
 
+use crate::ui::core::state::OccupiedScreenSpace;
+
 #[derive(Default, Resource)]
-pub struct ViewportState {}
+pub struct ViewportState {
+    last_updated_ui_occupied_screen_space: OccupiedScreenSpace
+}
+
+#[allow(dead_code)]
+impl ViewportState {
+    pub fn last_updated_ui_occupied_screen_space(&self) -> &OccupiedScreenSpace {
+        &self.last_updated_ui_occupied_screen_space
+    }
+    
+    pub fn last_updated_ui_occupied_screen_space_mut(&mut self) -> &mut OccupiedScreenSpace {
+        &mut self.last_updated_ui_occupied_screen_space
+    }
+    
+    pub fn set_last_updated_ui_occupied_screen_space(&mut self, last_updated_ui_occupied_screen_space: OccupiedScreenSpace) {
+        self.last_updated_ui_occupied_screen_space = last_updated_ui_occupied_screen_space;
+    }
+}
