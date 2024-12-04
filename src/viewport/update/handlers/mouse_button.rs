dca57b97019a8a8e5bfd6bb96cc6d8c41c8fecb1
Tretiakov Yehor <egorken3v@gmail.com>
2024-12-04 13:03:48 +0200

    feature/BTH-34: Fix mouse viewport ineraction (mouse cursor should be inside the viewport)

--- src/viewport/update/handlers/mouse_button.rs
+++ src/viewport/update/handlers/mouse_button.rs
@@ -5,7 +5,9 @@ use bevy::input::mouse::MouseMotion;
 
 use bevy::app::Plugin;
 use bevy::app::Update;
+use bevy::window::PrimaryWindow;
 
+use crate::ui::core::state::UIState;
 use crate::viewport::core::camera::ViewportCamera;
 
 pub struct MouseButtonHandler;
@@ -20,9 +22,34 @@ impl Plugin for MouseButtonHandler {
 }
 
 fn mouse_right_button_hold(
+    // mut cursor_moved_event_reader: EventReader<CursorMoved>,
     mut mouse_motion_event_reader: EventReader<MouseMotion>,
+    primary_window_query: Query<&Window, With<PrimaryWindow>>,
+    ui_state: Res<UIState>,
     mut viewport_camera_transform_query: Query<&mut Transform, With<ViewportCamera>>,
 ) {
+    let cursor_position = match primary_window_query.single().cursor_position() {
+        Some(position) => position,
+        None => return,
+    };
+
+    let window_width = primary_window_query.single().width();
+    let window_height = primary_window_query.single().height();
+
+    let ui_occupied_screen_space = ui_state.occupied_space();
+
+    if cursor_position.x < ui_occupied_screen_space.left()
+        || cursor_position.x > window_width - ui_occupied_screen_space.right()
+    {
+        return;
+    }
+
+    if cursor_position.y < ui_occupied_screen_space.top()
+        || cursor_position.y > window_height - ui_occupied_screen_space.top()
+    {
+        return;
+    }
+
     let mut transform = match viewport_camera_transform_query.get_single_mut() {
         Ok(transform) => transform,
         _ => unreachable!(),
