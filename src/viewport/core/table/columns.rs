dae808b5864d6a570f92da37c5decac526f33380
Tretiakov Yehor <egorken3v@gmail.com>
2024-12-06 17:29:27 +0200

    feature/BTH-52: Add table columns component and ui addition tool

---
+++ src/viewport/core/table/columns.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::Component;
+
+#[derive(Component, Default)]
+pub struct TableColumnsComponent {
+    pub columns: Vec<TableColumn>,
+}
+
+#[derive(Default, Debug)]
+pub struct TableColumn {
+    pub name: String,
+    pub data_type: DataType,
+    pub primary_key: bool,
+    pub nullable: bool,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum DataType {
+    None,
+    Integer,
+    Float,
+    String,
+    Boolean,
+    Date,
+}
+
+impl Default for DataType {
+    fn default() -> Self {
+        DataType::None
+    }
+}
